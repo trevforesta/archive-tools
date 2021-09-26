44caa62bb04b86380cdc850ac363e5104396b7cc
Trevor Foresta <trevforesta@gmail.com>
2021-09-25 22:55:42 -0400

    Update archive_tools.py

--- archive_tools.py
+++ archive_tools.py
@@ -9,27 +9,30 @@
 import time
 import duplicate_checker, format_img, get_random_file
 
-# Constructor
-def __init__(self, path):
-    self.path = path  
+class ArchiveTools:
+    # Constructor
+    def __init__(self, path):
+        self.path = path  
 
-def intro():
-    print('___________________')
-    print('\n* Archive Tools *')
-    print('___________________')
-    time.sleep(1)
-    print('\nAvailable Tools: ')
-    time.sleep(.1)
-    print('• Duplicate Checker (d)')
-    time.sleep(.1)
-    print('• Format to ISO 8601 (f)')
-    time.sleep(.1)
-    print('• Get Metadata (m)')
-    time.sleep(.1)
-    print('• Random File (r)')
-    time.sleep(1)
-    self.path = input('\n Set Archive Path: ')
+    def intro():
+        print('___________________')
+        print('\n* Archive Tools *')
+        print('___________________')
+        time.sleep(1)
+        self.path = input('\n Set Archive Path: ')
+        
+        print('\nAvailable Tools: ')
+        time.sleep(.1)
+        print('• Duplicate Checker (d)')
+        time.sleep(.1)
+        print('• Format to ISO 8601 (f)')
+        time.sleep(.1)
+        print('• Get Metadata (m)')
+        time.sleep(.1)
+        print('• Random File (r)')
+        time.sleep(1)
+        
 
-if __name__ == '__main__':
-    intro()
+    if __name__ == '__main__':
+        intro()
   
